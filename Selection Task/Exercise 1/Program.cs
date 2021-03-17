ec0c7d6e47cdad1f1e463cc8c79454e3922b19c4
maxbarrah <79820153+maxbarrah@users.noreply.github.com>
2021-03-17 15:17:15 +1100

    selection task exercise 1

---
+++ Selection Task/Exercise 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Age.");
+            string age = Console.ReadLine();
+            int ageNum = int.Parse(age);
+
+            // Selection if
+            if (ageNum >= 16) {
+                Console.WriteLine("Legally Adult Age.");
+            }
+            else {
+                Console.WriteLine("Legally Not adult Age.");
+            }
+        }
+    }
+}
