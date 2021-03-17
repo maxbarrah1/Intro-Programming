6f4522852a4e355b5be177bb907cbd27171a3c6b
maxbarrah <79820153+maxbarrah@users.noreply.github.com>
2021-03-17 13:46:18 +1100

    Selection Task Finish

---
+++ Selection Task/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Intro_Programming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Username.");
+            string username = Console.ReadLine();
+            
+
+            //selection if
+            if (username =="maxbarrah")
+            {
+                Console.WriteLine("Checking username...");
+                Console.WriteLine("Success! Please Enter Password.");
+                string password = (Console.ReadLine());
+
+                //Chain selection if
+                if (password =="password1") {
+                    Console.WriteLine("Checking Password...");
+                    Console.WriteLine("Login Succesful!"); 
+                }
+                
+                //Chain selection else
+                else {
+                    Console.WriteLine("login Unsuccessful, Please Try Again."); 
+                }
+
+            }
+            //selection else
+            else {
+            
+                Console.WriteLine("Incorrect Username, Please Try Again."); 
+            } 
+        }
+
+    }
+}
