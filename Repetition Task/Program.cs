56149ed89b89d1defad111117ad4c9a85860139d
maxbarrah <79820153+maxbarrah@users.noreply.github.com>
2021-03-25 16:14:42 +1100

    add repetition task

---
+++ Repetition Task/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Repetition_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Please Enter number of repetitions");
+            string input2 = Console.ReadLine();
+            int number = int.Parse(input2);
+            
+            int counter = 0;
+            
+
+
+            while ( counter < number ) {
+                
+                 Console.WriteLine("Please enter a name: ");
+                 string input = Console.ReadLine();
+                
+
+            }
+
+            System.Console.WriteLine("Goodbye");
+        }
+    }
+}
