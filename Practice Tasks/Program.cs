2eec971c1c52ffa6cea9c41bd198a6e9045cce76
maxbarrah <79820153+maxbarrah@users.noreply.github.com>
2021-04-09 15:06:51 +1000

    completed FizzBuzz

---
+++ Practice Tasks/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Practice_Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Name:");
+            string name = Console.ReadLine(); 
+            int counter = 0;
+            counter++;
+            
+            while (counter < 10) {
+                int counter2 = 0;
+                
+                while (counter2 < 10) {
+                    Console.WriteLine(name);
+                    counter2 = counter2 + 1; 
+                
+                 
+
+                 Console.WriteLine(counter);
+                    counter = counter + 1;
+                }
+
+        
+            
+            Console.WriteLine("Goodbye");
+            }
+        }
+    }
+}
--- Repetition Task/Program.cs
+++ Repetition Task/Program.cs
@@ -6,21 +6,34 @@ namespace Repetition_Task
     {
         static void Main(string[] args)
         {
-             //where counter begins
-             for (int counter = 1; counter <=100;){
-             //create repetition
-             counter++;
-             //create Fizz Buzz variables
-                 Console.WriteLine(counter);
-                if (counter % 3 == 0){
-                    Console.WriteLine("Fizz");}
-                if (counter % 5 == 0){
-                    Console.WriteLine("Buzz");}
-                if (counter % 3 == 0 && counter % 5 == 0){
-                    Console.WriteLine("FizzBuzz");}
-             }
-                  
-            
+            //where counter begins
+            int counter = 0;
+            while (counter <= 100)
+            {
+
+                //create Fizz Buzz variables
+                Console.WriteLine(counter);
+
+                if (counter % 3 == 0) 
+                {
+                    Console.WriteLine("Fizz");
+                }
+                if (counter % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                if (counter % 3 == 0 && counter % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else 
+                {
+                    Console.WriteLine(counter);
+                }
+                counter++;
+            }
+
+
 
         }
     }
