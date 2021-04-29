a3824fdd40b13004f78a71e4b50ce5a2f3018ce8
maxbarrah <79820153+maxbarrah@users.noreply.github.com>
2021-04-29 15:24:22 +1000

    practice assessment

---
+++ Practice Assessment/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practice_Assessment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            Random rand = new Random();
+            int num = rand.Next(0,25);
+            int guesses = 7;
+            Console.WriteLine("Hey there! Lets play a guessing game.");
+            Console.WriteLine("Guess a number between 0 and 25");
+            while (guesses > 0) 
+            {
+                int guess = int.Parse(Console.ReadLine());
+                list.Add(guess);
+
+                if (guess > num) {
+                    Console.WriteLine("Nope its less than that");
+                    Console.WriteLine("Number of guesses remaining: " + guesses);
+                    Console.WriteLine("Try Again!");
+                }
+
+                if (guess < num) {
+                    Console.WriteLine("Nope its greater than that");
+                    Console.WriteLine("Number of guesses remaining: " + guesses);
+                    Console.WriteLine("Try Again!");
+                }
+                
+                if (guess == num) {
+                    Console.WriteLine("Congrats! you win");
+                    Console.WriteLine("Your guess log:"); 
+                    foreach(int anything in list) 
+                    Console.Write("["+anything+"]" );
+                }
+                
+
+               
+               guesses--;
+
+                
+            }
+            
+                Console.WriteLine("You Lose");
+                Console.WriteLine("The number was " + num);
+                Console.WriteLine("Your guess log:");
+                foreach(int anything in list) 
+                Console.Write("["+anything+"]" );
+            
+        }
+    }
+}
---
+++ test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int randomNumber = new Random().Next(1, 11);
+ 
+        Console.WriteLine("I'm thinking of a number between 1 and 10. Can you guess it?");
+        while(true)
+        {
+            Console.Write("Guess: ");
+            if (int.Parse(Console.ReadLine()) == randomNumber)
+                break;
+            Console.WriteLine("That's not it. Guess again.");
+        }
+        Console.WriteLine("Congrats!! You guessed right!");
+    }
+    }
+}
